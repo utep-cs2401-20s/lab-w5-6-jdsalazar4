bdd1ccf9c3d9f5222452603e51a4ae69035074e1
jdsalazar4 <josephsalazar824@gmail.com>
2020-02-24 14:48:18 -0700

    Added methods to the Snake game and successfully fount out how to commit and push!

--- Java/src/SnakeGame.java
+++ Java/src/SnakeGame.java
@@ -1,3 +1,32 @@
 public class SnakeGame {
-    
+    private boolean game[][];
+    private int position[];
+
+    //Constructor for creating the empty gameboard
+    public SnakeGame(){
+        this.game = new boolean[1][1];
+    }
+
+    //constructor for finding the positions head
+    public SnakeGame (boolean[][] game, int x, int y){
+        this.game = game;
+        position[0]=x;
+        position[1]=y;
+    }
+
+    private static int exhaustiveChecks;
+
+    private static int recursiveChecks;
+
+    public int[] findTailExhaustive(int x, int y){
+        int[] snakeResult = {0,0,0};
+        int neighbor=0;
+
+        for(int i=0; i< game.length; i++){
+            for(int j=0; j< game.length; j++){
+                
+            }
+        }
+    }
+
 }
